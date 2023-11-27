c346d6e7f62128933b175ecbaf358e4d9a80198c
cykim8811 <cykim8811@snu.ac.kr>
2023-11-27 17:47:03 +0000

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     zip_safe=False,
     license='MIT'
 )
