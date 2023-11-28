8aa6598a76aab088a47077f3d6a441a0b1ab6967
cykim8811 <cykim8811@snu.ac.kr>
2023-11-28 03:41:24 +0000

    Update package name to upload to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyx-framework',
+    name='pyxframework',
     version='0.0.1',
     description='A framework that enables Python objects to be easily rendered on a web server',
     author='Kim Changyeon',
